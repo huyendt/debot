440cc684a0d14a3d8e88d6538f5298c808460544
Huyen Duong <huyendt1911@gmail.com>
2014-09-05 16:57:23 +0700

    Add Email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,17 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  # Don't actually send emails
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com" ,
+    :port => 587,
+    :domain => "smtp.gmail.com" ,
+    :authentication => "login" ,
+    :user_name => "huyendt@telsoft.com.vn" ,
+    :password => "Do191188@" ,
+    :enable_starttls_auto => true
+  } 
 end
