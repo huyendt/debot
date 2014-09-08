58d57279db2acc01592712d154e644d499ff15f0
Huyen Duong <huyendt1911@gmail.com>
2014-09-08 10:58:06 +0700

    Add Internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -13,26 +13,28 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
-  resources :users
+  scope '(:locale)' do
+    resources :users
 
-  resources :orders
+    resources :orders
 
-  resources :line_items
+    resources :line_items
 
-  resources :carts
+    resources :carts
 
-  get 'store/index'
+    get 'store/index'
 
-  resources :products do
-    get :who_bought, :on => :member
+    resources :products do
+      get :who_bought, :on => :member
+    end
+    # The priority is based upon order of creation: first created -> highest priority.
+    # See how all your routes lay out with "rake routes".
+
+    # You can have the root of your site routed with "root"
+    # root 'welcome#index'
+
+    root :to => 'store#index' , :as => 'store'
   end
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-  root :to => 'store#index' , :as => 'store'
 
 # Example of regular route:
 #   get 'products/:id' => 'catalog#view'
